43da12fc863005601c54be14bdeb108429b455bd
Maxim Starikov <mstarikov@elance-odesk.com>
2020-08-29 15:30:14 +0200

    new version

--- firmware/smartbike/config_common.h
+++ firmware/smartbike/config_common.h
@@ -1,12 +1,12 @@
 //common configuration paramenters
 
-#define FIRMWARE_VERSION 6
+#define FIRMWARE_VERSION 7
 
 //should be moved to flash memory later 
 #define DEVICE_TOKEN "e1a77e8b-f5d2-4b5d-bf0e-0d58943719c0"
 
-/* Time ESP32 will go to sleep (in seconds) 600 seconds = 60 minutes */
-#define TIME_TO_SLEEP  3600
+/* Time ESP32 will go to sleep (in seconds) 1800 seconds = 30 minutes */
+#define TIME_TO_SLEEP  1800
 
 #define GSM_TX 12
 #define GSM_RX 25
